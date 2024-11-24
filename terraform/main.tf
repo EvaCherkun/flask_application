bfbca648cb2455963ad0ddaadfb76b6d3c28e7df
EvaCherkun <evacherkun1@gmail.com>
2024-11-24 18:14:37 +0200

    change structure

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
 
-
-backend "s3" {
-  bucket         = "lab6-my-tf-state"
-  key            = "terraform.tfstate"
-  region         = "eu-north-1"
-  dynamodb_table = "lab6-my-tf-lockid"
+  backend "s3" {
+    bucket         = "lab6-my-tf-state"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "lab6-my-tf-lockid"
+  }
 }
 
 
