17c18fff0d167cbda20080dd13fe5f311eca2105
EvaCherkun <evacherkun1@gmail.com>
2024-11-29 15:45:52 +0200

    remove creation of security group and use existing one in terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,40 +19,6 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "random_id" "suffix" {
-  byte_length = 8
-}
-
-resource "aws_security_group" "web_app_sg" {
-  name        = "web_app_sg-${random_id.suffix.hex}"
-  description = "Security group for web app"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web_app_sg"
-  }
-}
-
 resource "aws_instance" "webapp_instance" {
   ami           = "ami-08eb150f611ca277f"
   instance_type = "t3.micro"
@@ -71,7 +37,8 @@ resource "aws_instance" "webapp_instance" {
     docker run -d -p 80:80 lunariiin/order_stack:latest
   EOF
 
-  vpc_security_group_ids = [aws_security_group.web_app_sg.id]
+  # security group ID
+  vpc_security_group_ids = ["sg-07ef34fd40ee02612"]
 
   tags = {
     Name = "webapp_instance"
